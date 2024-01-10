fac9104af0a2225b99877042842d264e85f8e199
220003 <220003pb@yse-c.net>
2024-01-10 11:33:39 +0900

    first

---
+++ app/components/navbar.tsx
@@ -0,0 +1,9 @@
+const navbar = () => {//nafeで基本的な構文が出る
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from '@/app/components/navbar'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +14,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ja">
+      <body >
+        {/* この書き方でNavbarを起動できる */}
+         <Navbar />
+        <main className='fiex min-h-screen fiex-col p-5'>
+        {children}
+        </main>
+      </body>
     </html>
   )
 }
